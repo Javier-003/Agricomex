af73225ff7bce2f41320cdeef6d293f83fa5b3f9
Javier-003 <141977702+Javier-003@users.noreply.github.com>
2024-02-10 02:11:34 -0600

    Delete agromex directory

--- agromex/src/conexion.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector
-conexion = mysql.connector.connect(
-        host="localhost",
-        port=3306,
-        user="root",
-        password="12345678",
-        db="AGRICOMEX"
-    )
